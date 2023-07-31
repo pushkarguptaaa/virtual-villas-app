d521a2298d76e220a6ee66c8f37f22d117a9a62d
pushkarguptaaa <pushkardps8@gmail.com>
2023-07-31 21:39:24 +0530

    Initial commit

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDBgLqp8xxGV-kb4Vbbxv199392QoYBZI",
+  authDomain: "virtual-villas-app.firebaseapp.com",
+  projectId: "virtual-villas-app",
+  storageBucket: "virtual-villas-app.appspot.com",
+  messagingSenderId: "78726273959",
+  appId: "1:78726273959:web:9a6062d85f48eeb662b482"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
