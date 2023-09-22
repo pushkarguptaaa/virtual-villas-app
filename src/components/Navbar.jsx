a617bf4ffbaf7a3e5b33d18c10751657ccbf9742
pushkarguptaaa <pushkardps8@gmail.com>
2023-09-22 21:17:23 +0530

    offers page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ function Navbar() {
                   ? 'navbarListItemNameActive'
                   : 'navbarListItemName'
                 }>
-                Offer</p>
+                Offers</p>
                 </li>
                 <li className='navbarListItem' onClick={() => navigate('/profile')}>
                 <PersonOutlineIcon
