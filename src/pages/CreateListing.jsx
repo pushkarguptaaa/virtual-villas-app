5c26ed9cbe15fbe819136e1c2e8e5c9f34c91dc2
pushkarguptaaa <pushkardps8@gmail.com>
2023-09-28 21:40:17 +0530

    get coords with geocoding api

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -64,20 +64,52 @@ function CreateListing() {
       const onSubmit = async (e) => {
         e.preventDefault()
     
-        // setLoading(true)
+        setLoading(true)
     
-        // if (discountedPrice >= regularPrice) {
-        //   setLoading(false)
-        //   toast.error('Discounted price needs to be less than regular price')
-        //   return
-        // }
+        if (discountedPrice >= regularPrice) {
+          setLoading(false)
+          toast.error('Discounted price needs to be less than regular price')
+          return
+        }
     
-        // if (images.length > 6) {
-        //   setLoading(false)
-        //   toast.error('Max 6 images')
-        //   return
-        // }
-        console.log(formData)
+        if (images.length > 6) {
+          setLoading(false)
+          toast.error('Max 6 images')
+          return
+        }
+        
+        let geolocation = {}
+    let location
+
+    if (geolocationEnabled) {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+      )
+
+      const data = await response.json()
+      console.log(data)
+
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+
+      location =
+        data.status === 'ZERO_RESULTS'
+          ? undefined
+          : data.results[0]?.formatted_address
+
+      if (location === undefined || location.includes('undefined')) {
+        setLoading(false)
+        toast.error('Please enter a correct address')
+        return
+      }
+
+    } else {
+      geolocation.lat = latitude
+      geolocation.lng = longitude
+      location= address
+    }
+    setLoading(false)
+
     }
 
     const onMutate = async (e) => {
