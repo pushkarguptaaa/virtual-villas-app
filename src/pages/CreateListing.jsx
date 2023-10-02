aa175275ba9282bf7431be59e18ca36d7f08e578
pushkarguptaaa <pushkardps8@gmail.com>
2023-10-02 19:54:09 +0530

    Save Listing to firestore

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -6,6 +6,7 @@ import {
     uploadBytesResumable,
     getDownloadURL,
   } from 'firebase/storage'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { useNavigate } from 'react-router-dom'
 import Spinner from '../components/Spinner'
@@ -166,9 +167,22 @@ function CreateListing() {
             return
           })
 
-    console.log(imgUrls)
+          const formDataCopy = {
+            ...formData,
+            imgUrls,
+            geolocation,
+            timestamp: serverTimestamp(),
+          }
+      
+          delete formDataCopy.images
+          delete formDataCopy.address
+          location && (formDataCopy.location = location)
+          !formDataCopy.offer && delete formDataCopy.discountedPrice
 
-    setLoading(false)
+          const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
+          setLoading(false)
+          toast.success('Listing saved')
+          navigate(`/category/${formDataCopy.type}/${docRef.id}`)
 
     }
 
