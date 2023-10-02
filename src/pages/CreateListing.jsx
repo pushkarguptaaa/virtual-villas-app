7f3d3eeff15c2d4e383db78dac4ad8899c89b1bd
pushkarguptaaa <pushkardps8@gmail.com>
2023-10-02 19:57:51 +0530

    location address fix

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -115,7 +115,6 @@ function CreateListing() {
     } else {
       geolocation.lat = latitude
       geolocation.lng = longitude
-      location= address
     }
 
     // Store image in firebase
@@ -174,9 +173,9 @@ function CreateListing() {
             timestamp: serverTimestamp(),
           }
       
+          formDataCopy.location = address
           delete formDataCopy.images
           delete formDataCopy.address
-          location && (formDataCopy.location = location)
           !formDataCopy.offer && delete formDataCopy.discountedPrice
 
           const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
