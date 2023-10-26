83824b5b52f8d3c6757909359d27cd0207b0d072
pushkarguptaaa <pushkardps8@gmail.com>
2023-10-26 11:48:14 +0530

    Listings page slider

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -99,7 +99,7 @@ function CreateListing() {
       console.log(data)
 
       geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
-      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+      geolocation.long = data.results[0]?.geometry.location.long ?? 0
 
       location =
         data.status === 'ZERO_RESULTS'
@@ -114,7 +114,7 @@ function CreateListing() {
 
     } else {
       geolocation.lat = latitude
-      geolocation.lng = longitude
+      geolocation.long = longitude
     }
 
     // Store image in firebase
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,11 +1,19 @@
 import { useState, useEffect } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import "swiper/css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/a11y';
 import { getDoc, doc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.config'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
     const [listing, setListing] = useState(null)
@@ -38,6 +46,21 @@ function Listing() {
 
   return (
     <main>
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: 'cover',
+                height: '25vw'
+              }}
+              className='swiperSlideDiv'
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
         <div
         className='shareIconDiv'
         onClick={() => {
