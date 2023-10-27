17d16f8ebd63ac15c9e757dbe809dcdb007a8d4e
pushkarguptaaa <pushkardps8@gmail.com>
2023-10-27 21:06:03 +0530

    removed console warnings

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -15,6 +15,7 @@ import { v4 as uuidv4 } from 'uuid'
 
 
 function CreateListing() {
+  // eslint-disable-next-line
     const [geolocationEnabled, setGeolocationEnabled] = useState(true)
     const [loading, setLoading] = useState(false)
     const [formData, setFormData] = useState({
